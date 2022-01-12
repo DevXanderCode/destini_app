67f156ca6106840cac12a26c9de8e1d9da9e6ff6
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-12 22:58:13 +0100

    created methods to get the choice 1 and 2 text

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -41,6 +41,14 @@ class StoryBrain {
   String getStory() {
     return _storyData[0].storyTitle;
   }
+
+  String getChoice1() {
+    return _storyData[0].choice1;
+  }
+
+  String getChoice2() {
+    return _storyData[0].choice2;
+  }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
