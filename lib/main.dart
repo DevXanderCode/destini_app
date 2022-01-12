0b3d45d41ec0c06459abe20883833ad0637522c4
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-12 23:14:04 +0100

    removed todo from 15 to 20

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'story_brain.dart';
-
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
 
 void main() => runApp(Destini());
 
@@ -42,7 +40,6 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
                     storyBrain.getStory(),
                     style: TextStyle(
                       fontSize: 25.0,
@@ -55,7 +52,7 @@ class _StoryPageState extends State<StoryPage> {
                 child: MaterialButton(
                   onPressed: () {
                     //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    storyBrain.nextStory(1);
                   },
                   color: Colors.red,
                   child: Text(
@@ -76,7 +73,7 @@ class _StoryPageState extends State<StoryPage> {
                 child: MaterialButton(
                   onPressed: () {
                     //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    storyBrain.nextStory(2);
                   },
                   color: Colors.blue,
                   child: Text(
--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,6 +1,7 @@
 import "story.dart";
 
 class StoryBrain {
+  int storyNumber = 0;
   List<Story> _storyData = [
     Story(
         storyTitle:
@@ -44,17 +45,13 @@ class StoryBrain {
   String getChoice2() {
     return _storyData[0].choice2;
   }
+
+  void nextStory(int choiceNumber) {}
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-
-//TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
 //TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
 
